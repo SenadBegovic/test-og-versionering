a5d9fe4d4962a536270d00964f37e93b5e3bf0ba
SenadBegovic <senadbegovic2500@hotmail.com>
2015-10-19 16:19:25 +0200

    Lavet en JUnitTest class samt testede getTotal test
    Testede getTotal metoden som skal erstatte DenTotaleIndkomstErForkert
    test

---
+++ src/automat/JUnitTest.java
@@ -0,0 +1,21 @@
+package automat;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JUnitTest {
+
+	@Test
+	public void testgetTotal() {
+		Billetautomat automat = new Billetautomat();
+		automat.montørLogin("1234");
+		automat.indsætPenge(90);
+		automat.udskrivBillet();
+		automat.setBilletpris(20);
+		automat.udskrivBillet();
+		int retur = automat.getTotal();
+		assertEquals(30, retur);
+	}
+
+}
